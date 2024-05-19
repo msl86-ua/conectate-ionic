63470a1c26c955f499000d45f913ce476f819b97
Francisco José Ramirez Paraíso <fjrp9@mscloud.ua.es>
2024-05-19 17:51:56 +0200

    Amigos y chat terminado

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { ChatPageRoutingModule } from './chat-routing.module';
 
 import { ChatPage } from './chat.page';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ChatPageRoutingModule
+    ChatPageRoutingModule,
+    RouterModule.forChild([{ path: '', component: ChatPage }])
   ],
   declarations: [ChatPage]
 })
--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -10,18 +10,29 @@ export class ChatPage implements OnInit {
 
   friendId: number = 0;
   friend: any;
+  conversations: any[] = [];
+  newMessage: string = '';
 
   friends = [
-    { id: 1, name: 'Pablo', img: 'assets/img/persona1.jpg' },
-    { id: 2, name: 'Lucia', img: 'assets/img/persona2.jpg' },
-    { id: 3, name: 'Antonio', img: 'assets/img/persona3.jpg' },
-    { id: 4, name: 'Manolo', img: 'assets/img/persona4.jpg' }
+    { id: 1, name: 'Pablo', img: 'assets/img/persona1.jpg', conversation: [{ sender: 'Pablo', message: '¡Hola! ¿Cómo estás?' }, { sender: 'You', message: 'Hola Pablo! Estoy bien, gracias ¿y tú?' }, { sender: 'Pablo', message: 'Genial también, ¿quieres hacer algo este fin de semana?' }] },
+    { id: 2, name: 'Lucia', img: 'assets/img/persona2.jpg', conversation: [{ sender: 'Lucia', message: '¡Hola! ¿Cómo estás?' }, { sender: 'You', message: 'Hola Lucia! Estoy bien, gracias ¿y tú?' }, { sender: 'Lucia', message: 'Genial también, ¿quieres hacer algo este fin de semana?' }] },
+    { id: 3, name: 'Antonio', img: 'assets/img/persona3.jpg', conversation: [{ sender: 'Antonio', message: '¡Hola! ¿Cómo estás?' }, { sender: 'You', message: 'Hola Antonio! Estoy bien, gracias ¿y tú?' }, { sender: 'Antonio', message: 'Genial también, ¿quieres hacer algo este fin de semana?' }] },
+    { id: 4, name: 'Manolo', img: 'assets/img/persona4.jpg', conversation: [{ sender: 'Manolo', message: '¡Hola! ¿Cómo estás?' }, { sender: 'You', message: 'Hola Manolo! Estoy bien, gracias ¿y tú?' }, { sender: 'Manolo', message: 'Genial también, ¿quieres hacer algo este fin de semana?' }] }
   ];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.friendId = +(this.route.snapshot.paramMap.get('friendId') ?? 0);
     this.friend = this.friends.find(f => f.id === this.friendId);
+    this.conversations = this.friend?.conversation || [];
   }
+
+  sendMessage() {
+    if (this.newMessage.trim()) {
+      this.conversations.push({ sender: 'You', message: this.newMessage });
+      this.newMessage = '';
+    }
+  }
+  
 }
